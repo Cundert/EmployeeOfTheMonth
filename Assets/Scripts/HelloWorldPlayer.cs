b6615025e7acb80223e1de05fe3f9e560568d64c
Samuel <samuel.alarcon@est.fib.upc.edu>
2021-05-15 01:43:47 +0200

    More testing

--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -10,11 +10,6 @@ namespace HelloWorld {
 		public Vector2 dir;
 		public float speed;
 
-		public NetworkVariableBool namesDisplayed = new NetworkVariableBool(new NetworkVariableSettings {
-			WritePermission=NetworkVariablePermission.Everyone,
-			ReadPermission=NetworkVariablePermission.Everyone
-		});
-
 		public NetworkVariableString PlayerName = new NetworkVariableString(new NetworkVariableSettings {
 			WritePermission=NetworkVariablePermission.ServerOnly,
 			ReadPermission=NetworkVariablePermission.Everyone
@@ -26,11 +21,11 @@ namespace HelloWorld {
 		});
 
 		private void OnEnable() {
-			namesDisplayed.OnValueChanged+=DisplayNames;
+			PlayerName.OnValueChanged+=DisplayNames;
 		}
 
 		private void OnDisable() {
-			namesDisplayed.OnValueChanged-=DisplayNames;
+			PlayerName.OnValueChanged-=DisplayNames;
 		}
 
 		public override void NetworkStart() {
@@ -43,19 +38,16 @@ namespace HelloWorld {
 			SubmitPositionRequestServerRpc(new Vector2(transform.position.x,transform.position.y));
 		}
 
-		void DisplayNames(bool before, bool after) {
-			if (!after) {
-				string childname = transform.GetChild(0).gameObject.GetComponent<TextMesh>().text;
-				if (childname!=PlayerName.Value) {
-					transform.GetChild(0).gameObject.GetComponent<TextMesh>().text=PlayerName.Value;
-				}
+		void DisplayNames(string before, string after) {
+			string childname = transform.GetChild(0).gameObject.GetComponent<TextMesh>().text;
+			if (childname!=after) {
+				transform.GetChild(0).gameObject.GetComponent<TextMesh>().text=after;
 			}
 		}
 
 		[ServerRpc]
 		public void SetPlayerNameServerRpc(string name, ServerRpcParams rpcParams = default) {
 			PlayerName.Value=name;
-			namesDisplayed.Value=false;
 		}
 
 		[ServerRpc]
@@ -78,7 +70,6 @@ namespace HelloWorld {
             dir = dir * val;
 			Move();
 			if (!IsLocalPlayer) transform.position=Position.Value;
-			namesDisplayed.Value=true;
 		}
 	}
 }
