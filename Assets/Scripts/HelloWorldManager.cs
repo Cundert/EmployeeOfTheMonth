01712226fdae244211fd9c5549c87e1c88510dcb
Samuel <samuel.alarcon@est.fib.upc.edu>
2021-05-15 01:24:32 +0200

    Names showing but bugged

--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -4,32 +4,46 @@ using UnityEngine;
 
 namespace HelloWorld {
 	public class HelloWorldManager : MonoBehaviour {
+
+		string playerName="Name";
+
 		void OnGUI() {
 			GUILayout.BeginArea(new Rect(10, 10, 300, 300));
 			if (!NetworkManager.Singleton.IsClient&&!NetworkManager.Singleton.IsServer) {
 				StartButtons();
+				playerName=GUILayout.TextField(playerName, 10);
 			} else {
-				StatusLabels();
-
+				StatusLabels(playerName);
 				SubmitNewPosition();
 			}
 
 			GUILayout.EndArea();
 		}
 
-		static void StartButtons() {
+
+		void StartButtons() {
 			if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
 			if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
 			if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
 		}
 
-		static void StatusLabels() {
+		static void StatusLabels(string playerName) {
 			var mode = NetworkManager.Singleton.IsHost ?
 				"Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
 
 			GUILayout.Label("Transport: "+
 				NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
 			GUILayout.Label("Mode: "+mode);
+			string pN="";
+			if (NetworkManager.Singleton.ConnectedClients.TryGetValue(NetworkManager.Singleton.LocalClientId,
+					out var networkedClient)) {
+				var player = networkedClient.PlayerObject.GetComponent<HelloWorldPlayer>();
+				if (player) {
+					pN=playerName;
+					player.SetPlayerNameServerRpc(pN);
+				}
+			}
+			GUILayout.Label("Name: "+pN);
 		}
 
 		static void SubmitNewPosition() {
--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -10,10 +10,25 @@ namespace HelloWorld {
 		public Vector2 dir;
 		public float speed;
 
+		public NetworkVariableBool namesDisplayed;
+
+		public NetworkVariableString PlayerName = new NetworkVariableString(new NetworkVariableSettings {
+			WritePermission=NetworkVariablePermission.ServerOnly,
+			ReadPermission=NetworkVariablePermission.Everyone
+		});
+
 		public NetworkVariableVector3 Position = new NetworkVariableVector3(new NetworkVariableSettings {
 			WritePermission=NetworkVariablePermission.ServerOnly,
 			ReadPermission=NetworkVariablePermission.Everyone
 		});
+
+		private void OnEnable() {
+			namesDisplayed.OnValueChanged+=DisplayNames;
+		}
+
+		private void OnDisable() {
+			namesDisplayed.OnValueChanged-=DisplayNames;
+		}
 
 		public override void NetworkStart() {
 			Move();
@@ -23,6 +38,22 @@ namespace HelloWorld {
 			if (!IsLocalPlayer) return;
 			transform.position+=new Vector3(dir.x, dir.y, 0);
 			SubmitPositionRequestServerRpc(new Vector2(transform.position.x,transform.position.y));
+		}
+
+		void DisplayNames(bool before, bool after) {
+			if (!after) {
+				string childname = transform.GetChild(0).gameObject.GetComponent<TextMesh>().text;
+				if (childname!=PlayerName.Value) {
+					transform.GetChild(0).gameObject.GetComponent<TextMesh>().text=PlayerName.Value;
+				}
+				namesDisplayed.Value=true;
+			}
+		}
+
+		[ServerRpc]
+		public void SetPlayerNameServerRpc(string name, ServerRpcParams rpcParams = default) {
+			PlayerName.Value=name;
+			namesDisplayed.Value=false;
 		}
 
 		[ServerRpc]
