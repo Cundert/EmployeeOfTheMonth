9e9ec8e80dfd6813a5209353993ac33ebb0b31c9
adriantormos <adriantormos98@gmail.com>
2021-05-15 00:48:47 +0200

    Encapsulate sections of player Update function
    Now the player's Update consists on calls to other function, more understandable at first glance.

--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -69,6 +69,35 @@ namespace HelloWorld {
 			
 		}
 
+        Vector2 getMovementVector(float val)
+        {
+            Vector2 dir = new Vector2(0, 0);
+            if (Input.GetKey("s")) dir += new Vector2(0, -1);
+            if (Input.GetKey("w")) dir += new Vector2(0, 1);
+            if (Input.GetKey("a")) dir += new Vector2(-1, 0);
+            if (Input.GetKey("d")) dir += new Vector2(1, 0);
+            dir.Normalize();
+            return dir * val;
+        }
+
+        Vector2 getAttackVector()
+        {
+            adir = new Vector2(0, 0);
+            if (Input.GetKey("down")) adir += new Vector2(0, -1);
+            if (Input.GetKey("up")) adir += new Vector2(0, 1);
+            if (Input.GetKey("left")) adir += new Vector2(-1, 0);
+            if (Input.GetKey("right")) adir += new Vector2(1, 0);
+            return adir;
+        }
+
+        void MoveCamera()
+        {
+            Vector3 serverPosition = Position.Value;
+            Vector3 possibleFuturePosition = new Vector3(serverPosition.x, serverPosition.y, -10);
+            if (Vector3.Distance(playerCamera.transform.position, possibleFuturePosition) > 0.05)
+                playerCamera.transform.position = possibleFuturePosition;
+        }
+
         void Start()
         {
             playerCamera = CameraController.instance;
@@ -81,28 +110,15 @@ namespace HelloWorld {
 			}
 			if (IsLocalPlayer) {
 				Timer += Time.deltaTime;
-				float val = speed*Time.deltaTime;
-				dir = new Vector2(0, 0);
-				if (Input.GetKey("s")) dir+=new Vector2( 0, -1);
-				if (Input.GetKey("w")) dir+=new Vector2( 0,  1);
-				if (Input.GetKey("a")) dir+=new Vector2(-1,  0);
-				if (Input.GetKey("d")) dir+=new Vector2( 1,  0);
-				dir.Normalize();
-				dir = dir * val;
-				
-				adir = new Vector2(0, 0);
-				if (Input.GetKey("down"))  adir+=new Vector2( 0, -1);
-				if (Input.GetKey("up"))    adir+=new Vector2( 0,  1);
-				if (Input.GetKey("left"))  adir+=new Vector2(-1,  0);
-				if (Input.GetKey("right")) adir+=new Vector2( 1,  0);
-				
-				Move();
+
+                Vector2 dir = getMovementVector(speed * Time.deltaTime);
+                Vector2 adir = getAttackVector();
+
+                Move();
 				Attack();
-        
-        Vector3 serverPosition = Position.Value;
-        Vector3 possibleFuturePosition = new Vector3(serverPosition.x, serverPosition.y, -10);
-        if (Vector3.Distance(playerCamera.transform.position, possibleFuturePosition) > 0.05)
-            playerCamera.transform.position = possibleFuturePosition;
+
+                MoveCamera();
+
 			} else {
 				transform.position=Position.Value;
 			}
