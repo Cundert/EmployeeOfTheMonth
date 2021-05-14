247451eb35ee6e2338f3a617bc8fb59f46a7e16d
Samuel <samuel.alarcon@est.fib.upc.edu>
2021-05-14 21:35:34 +0200

    Reduces FPS limit to 30

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -7,6 +7,6 @@ public class SettingsScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		Application.targetFrameRate=60;
+		Application.targetFrameRate=30;
 	}
 }
