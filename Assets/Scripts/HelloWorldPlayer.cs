93b5276b84fa08e72cfb8b65307fecad75340780
hectobreak <40203983+hectobreak@users.noreply.github.com>
2021-05-14 23:11:14 +0200

    Update HelloWorldPlayer.cs

--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -7,10 +7,21 @@ using UnityEngine;
 namespace HelloWorld {
 	public class HelloWorldPlayer : NetworkBehaviour {
 
-		public Vector2 dir;
+		public Vector2 dir, adir;
+		public int nattacks;
 		public float speed;
 
 		public NetworkVariableVector3 Position = new NetworkVariableVector3(new NetworkVariableSettings {
+			WritePermission=NetworkVariablePermission.ServerOnly,
+			ReadPermission=NetworkVariablePermission.Everyone
+		});
+		
+		public NetworkVariableVector3 AttackDir = new NetworkVariableVector3(new NetworkVariableSettings {
+			WritePermission=NetworkVariablePermission.ServerOnly,
+			ReadPermission=NetworkVariablePermission.Everyone
+		});
+		
+		public NetworkVariableInt NAttacks = new NetworkVariableInt(new NetworkVariableSettings {
 			WritePermission=NetworkVariablePermission.ServerOnly,
 			ReadPermission=NetworkVariablePermission.Everyone
 		});
@@ -22,29 +33,56 @@ namespace HelloWorld {
 		public void Move() {
 			if (!IsLocalPlayer) return;
 			transform.position+=new Vector3(dir.x, dir.y, 0);
-			SubmitPositionRequestServerRpc(new Vector2(transform.position.x,transform.position.y));
+			UpdatePositionServerRpc(transform.position);
+		}
+		
+		public void Attack(){
+			if(!IsLocalPlayer) return;
+			UpdateAttackServerRpc(adir);
 		}
 
 		[ServerRpc]
-		void SubmitPositionRequestServerRpc(Vector2 d, ServerRpcParams rpcParams = default) {
-			Position.Value=new Vector3(d.x, d.y, 0);
+		void UpdatePositionServerRpc(Vector3 d, ServerRpcParams rpcParams = default) {
+			Position.Value=d;
 		}
-
-		Vector3 GetRandomPositionOnPlane() {
-			return new Vector3(dir.x, dir.y, 0);
+		
+		[ServerRpc]
+		void UpdateAttackServerRpc(Vector3 a, ServerRpcParams rpcParams = default) {
+			AttackDir.Value=a;
+			NAttacks.Value++;
+		}
+		
+		void generateAttack(){
+			// Generates an attack in direction AttackDir
+			// todo
 		}
 
 		void Update() {
-			float val = speed*Time.deltaTime;
-            dir = new Vector2(0, 0);
-            if (Input.GetKey("s")) dir+=new Vector2(0, -1);
-			if (Input.GetKey("w")) dir+=new Vector2(0, 1);
-			if (Input.GetKey("a")) dir+=new Vector2(-1, 0);
-			if (Input.GetKey("d")) dir+=new Vector2(1, 0);
-            dir.Normalize();
-            dir = dir * val;
-			Move();
-			if (!IsLocalPlayer) transform.position=Position.Value;
+			while(nattacks < NAttacks.Value){
+				++nattacks;
+				generateAttack();
+			}
+			if (IsLocalPlayer) {
+				float val = speed*Time.deltaTime;
+				dir = new Vector2(0, 0);
+				if (Input.GetKey("s")) dir+=new Vector2( 0, -1);
+				if (Input.GetKey("w")) dir+=new Vector2( 0,  1);
+				if (Input.GetKey("a")) dir+=new Vector2(-1,  0);
+				if (Input.GetKey("d")) dir+=new Vector2( 1,  0);
+				dir.Normalize();
+				dir = dir * val;
+				
+				adir = new Vector2(0, 0);
+				if (Input.GetKey("down"))  adir+=new Vector2( 0, -1);
+				if (Input.GetKey("up"))    adir+=new Vector2( 0,  1);
+				if (Input.GetKey("left"))  adir+=new Vector2(-1,  0);
+				if (Input.GetKey("right")) adir+=new Vector2( 1,  0);
+				
+				Move();
+				Attack();
+			} else {
+				transform.position=Position.Value;
+			}
 		}
 	}
 }
