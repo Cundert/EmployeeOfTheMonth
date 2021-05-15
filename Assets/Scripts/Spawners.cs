0c647a4aa4d3765592f0e441cbac65420a05b178
hectobreak <40203983+hectobreak@users.noreply.github.com>
2021-05-16 00:10:38 +0200

    Spawner, y trabajo de chinos

--- Assets/Scripts/Spawners.cs
+++ Assets/Scripts/Spawners.cs
@@ -11,6 +11,12 @@ using System.Linq;
 public class Spawners : MonoBehaviour
 {
 	public List<GameObject> spawnPositions;
+	
+	
+	public NetworkVariableInt Readies = new NetworkVariableInt(new NetworkVariableSettings {
+		WritePermission=NetworkVariablePermission.ServerOnly,
+		ReadPermission=NetworkVariablePermission.Everyone
+	});
 
 	public NetworkVariableBool HasBeenShuffled = new NetworkVariableBool(new NetworkVariableSettings {
 		WritePermission=NetworkVariablePermission.ServerOnly,
@@ -23,18 +29,27 @@ public class Spawners : MonoBehaviour
 			HasBeenShuffled.Value=true;
 			spawnPositions=spawnPositions.OrderBy(x => Random.value).ToList();
 		}
+		Readies.Value++;
+	}
+	
+	[ServerRpc]
+	void IAmReadyServerRpc(ServerRpcParams rpcParams = default){
+		Readies.Value++;
 	}
 
 	// Start is called before the first frame update
 	void Start()
     {
-		ShuffleSpawnsServerRpc();
+		if(NetworkManager.Singleton.ServerClientId == NetworkManager.Singleton.LocalClientId)
+			ShuffleSpawnsServerRpc();
+		else
+			IAmReadyServerRpc();
 
 		if (NetworkManager.Singleton.ConnectedClients.TryGetValue(NetworkManager.Singleton.LocalClientId,
 					out var networkedClient)) {
 			var player = networkedClient.PlayerObject.GetComponent<HelloWorldPlayer>();
 			if (player) {
-				while (!HasBeenShuffled.Value) ;
+				while (Readies.Value < NetworkManager.Singleton.ConnectedClients.Count()) ;
 				int i = (int)NetworkManager.Singleton.LocalClientId;
 				player.SetPosition(spawnPositions[i].transform.position);
 			}
