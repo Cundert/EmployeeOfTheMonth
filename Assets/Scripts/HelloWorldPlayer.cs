2a0486e361625d90e2c398e6e3a5bbd3f03a9ff1
adriantormos <adriantormos98@gmail.com>
2021-05-15 14:16:18 +0200

    Make player-obstacle collisions behave correctly
    Now Player doesn't stutter when colliding with anything. Its movement also slides when colliding with the obstacle.
    
    To do: detect that the collision is of correct layer, obstacle.

--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -17,6 +17,8 @@ namespace HelloWorld {
 		public float Timer = 0.0f;
 		
 		public GameObject AttackObject;
+
+		public int amountOfObstacleCollisions = 0;
     
 		public NetworkVariableString PlayerName = new NetworkVariableString(new NetworkVariableSettings {
 			WritePermission=NetworkVariablePermission.ServerOnly,
@@ -63,9 +65,10 @@ namespace HelloWorld {
 			InitializeHPServerRpc(20);
 		}
 
-		public void Move() {
+		public void Move()
+		{
 			if (!IsLocalPlayer) return;
-			transform.position+=new Vector3(dir.x, dir.y, 0);
+			transform.position += new Vector3(dir.x, dir.y, 0);
 			UpdatePositionServerRpc(transform.position);
 		}
 		
@@ -114,12 +117,12 @@ namespace HelloWorld {
             HP.Value += HPDiff;
         }
 		
-	void generateAttack(){
-		// Generates an attack in direction AttackDir
-		// todo
-		GameObject bullet = Instantiate(AttackObject, transform.position, Quaternion.FromToRotation(new Vector3(1, 0, 0), AttackDir.Value));
-		bullet.GetComponent<BulletScript>().source = gameObject;
-	}
+		void generateAttack(){
+			// Generates an attack in direction AttackDir
+			// todo
+			GameObject bullet = Instantiate(AttackObject, transform.position, Quaternion.FromToRotation(new Vector3(1, 0, 0), AttackDir.Value));
+			bullet.GetComponent<BulletScript>().source = gameObject;
+		}
 
         Vector2 getMovementVector(float val)
         {
@@ -128,8 +131,26 @@ namespace HelloWorld {
             if (Input.GetKey("w")) dir += new Vector2(0, 1);
             if (Input.GetKey("a")) dir += new Vector2(-1, 0);
             if (Input.GetKey("d")) dir += new Vector2(1, 0);
-            dir.Normalize();
-            return dir * val;
+
+			if (Physics2D.CircleCast(
+					transform.position,
+					GetComponent<CircleCollider2D>().radius,
+					new Vector3(dir.x, 0, 0),
+					val,
+					LayerMask.GetMask("TestObstacle")
+				).collider != null)
+				dir.x = 0;
+			if (Physics2D.CircleCast(
+					transform.position,
+					GetComponent<CircleCollider2D>().radius,
+					new Vector3(0, dir.y, 0),
+					val,
+					LayerMask.GetMask("TestObstacle")
+				).collider != null)
+				dir.y = 0;
+
+			dir.Normalize();
+			return dir * val;
         }
 
         Vector2 getAttackVector()
@@ -146,7 +167,7 @@ namespace HelloWorld {
         {
             Vector3 serverPosition = Position.Value;
             Vector3 possibleFuturePosition = new Vector3(serverPosition.x, serverPosition.y, -10);
-            if (Vector3.Distance(playerCamera.transform.position, possibleFuturePosition) > 0.05)
+            if (Vector3.Distance(playerCamera.transform.position, possibleFuturePosition) > 0.04)
                 playerCamera.transform.position = possibleFuturePosition;
         }
 
@@ -156,6 +177,8 @@ namespace HelloWorld {
         }
 
 		void Update() {
+			Debug.Log(amountOfObstacleCollisions);
+
             if (HPHasBeenSet.Value && HP.Value == 0)
                 Destroy(gameObject);
 
@@ -164,30 +187,33 @@ namespace HelloWorld {
 				generateAttack();
 			}
 
-			if (IsLocalPlayer) {
+			if (IsLocalPlayer)
+			{
 				Timer += Time.deltaTime;
+				float deltaMovement = speed * Time.deltaTime;
 
-				dir = getMovementVector(speed * Time.deltaTime);
+				dir = getMovementVector(deltaMovement);
+				Move();
+				MoveCamera();
+
+
 				adir = getAttackVector();
 
-				Move();
 				Attack();
-
-				MoveCamera();
 
             } else {
 				transform.position=Position.Value;
 			}
 		}
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-      // If the Player has collided with a bullet that isn't theirs, and the Player is the local one
-      // Then reduce the Player's health
-      if (other.gameObject.tag == "Bullet" && other.GetComponent<BulletScript>().source != gameObject && IsLocalPlayer) {
-        UpdateHPServerRpc(other.GetComponent<BulletScript>().BulletDamage*-1);
-      }
-    }
+		void OnTriggerEnter2D(Collider2D other)
+		{
+			// If the Player has collided with a bullet that isn't theirs, and the Player is the local one
+			// Then reduce the Player's health
+			if (other.gameObject.tag == "Bullet" && other.GetComponent<BulletScript>().source != gameObject && IsLocalPlayer) {
+				UpdateHPServerRpc(other.GetComponent<BulletScript>().BulletDamage*-1);
+			}
+		}
 
 	}
 }
